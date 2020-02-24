0a30dcfc57f18070288ccdeb30ca7569682b666b
Noureddine Laassari <nlaassari@gmail.com>
2020-02-24 12:41:50 +0100

    fix user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
 
   validates :email, presence: true, uniqueness: true
   validates :first_name, length:{ minimum:3, maximum: 30}
-  validates :last_name, { minimum:3, maximum: 30}
-  validates :password_digest, presence: true, uniqueness: true, { minimum:8, maximum: 60}
+  validates :last_name,  length:{ minimum:3, maximum: 30}
+  validates :password_digest, presence: true, uniqueness: true, length: { minimum:8, maximum: 60}
 end
